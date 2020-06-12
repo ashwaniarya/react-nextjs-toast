2fafe4ba92ed8de3846d1d6345b64712fa70316a
Ashwani Arya <ashwaniarya@Administrators-MacBook-Pro.local>
2020-06-12 16:26:24 +0530

    Web pack config to build react page

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,78 @@
 // Webpack config
-var path = require('path')
-
-module.exports = {
-  mode: 'production',
-  entry: './src/toast.js',
-  output: {
-      path: path.resolve('lib'),
-      filename: 'toast.js',
-      libraryTarget: 'commonjs2'
-  },
-  module: {
-      rules: [
-          {
-              test: /\.jsx?$/,
-              exclude: /(node_modules)/,
-              use: 'babel-loader'
-          }
-      ]
-  }
+const path = require('path')
+const webpack = require('webpack');
+module.exports = (env, argv) => {
+    console.log(env)
+    console.log(argv)
+    if(argv.mode === "development") {
+        return module.exports = {
+            entry: "./src/index.js",
+            mode: "production",
+            module: {
+              rules: [
+                {
+                  test: /\.(js|jsx)$/,
+                  exclude: /(node_modules|bower_components)/,
+                  loader: "babel-loader",
+                }
+              ]
+            },
+            resolve: { extensions: ["*", ".js", ".jsx"] },
+            output: {
+              path: path.resolve(__dirname, "dist/"),
+              publicPath: "/dist/",
+              filename: "bundle.js"
+            },
+            devServer: {
+              contentBase: path.join(__dirname, "public/"),
+              port: 3000,
+              publicPath: "http://192.168.0.102:3000/dist/",
+              hotOnly: true
+            },
+            plugins: [new webpack.HotModuleReplacementPlugin()]
+          };
+    }   
+    if(argv.mode === 'production') {
+      if(argv.type === 'package') {
+        return {
+            mode: 'production',
+            entry: './src/toast.js',
+            output: {
+                path: path.resolve('lib'),
+                filename: 'toast.js',
+                libraryTarget: 'commonjs2'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.jsx?$/,
+                        exclude: /(node_modules)/,
+                        use: 'babel-loader'
+                    }
+                ]
+            }
+        }
+      }
+      if(argv.type === "demo") {
+          return module.exports = {
+            entry: "./src/index.js",
+            mode: "production",
+            module: {
+              rules: [
+                {
+                  test: /\.(js|jsx)$/,
+                  exclude: /(node_modules|bower_components)/,
+                  loader: "babel-loader",
+                }
+              ]
+            },
+            resolve: { extensions: ["*", ".js", ".jsx"] },
+            output: {
+              path: path.resolve(__dirname, "dist/"),
+              publicPath: "/dist/",
+              filename: "bundle.js"
+            }
+          };
+      }
+    }
 }
